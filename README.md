f72a349c05677f27550fb6146cc3a8baa3d5893a
Mike Bellika <mike@bellika.dk>
2024-11-08 11:52:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Noice scrollbar issue reproduction
+
+To run first install dependencies:
+
+```bash
+pnpm i
+```
+
+Run the dev server to generate types
+
+```bash
+pnpm dev
+```
+
+Might need to restart nvim or LspRestart after that.
+
+Then go to /components/ReproWatch.vue and do like the video
+
+```
+
+```
+
+```
+
+```
---
+++ components/ReproWatch.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const { hello } = defineProps<{
+  hello: {
+    user: string;
+    password: string;
+    a: string;
+    b: string;
+    c: string;
+    d: string;
+    e: string;
+    f: string;
+    a1: string;
+    b1: string;
+    c1: string;
+    d1: string;
+    e1: string;
+    f1: string;
+  };
+}>();
+
+watch(hello, (newHello) => {
+  console.log(newHello);
+});
+</script>
+<template>Hello world</template>
